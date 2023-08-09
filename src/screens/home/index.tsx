9a2c7edf75e9d145e63c9fdf3070a5ef39d1612d
Igor Barros <igorbarrosib@gmail.com>
2023-08-09 16:56:16 -0300

    alterando estrutura

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, TextInput, TouchableOpacity, View } from "react-native";
 import styles from './styles';
 
 
 export function Home(){
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Nome do Evento</Text>
+        <Text style={styles.h1}>Eventos</Text>
+
+        <View style={styles.contents}>
+          <TextInput 
+            style={styles.input} 
+            placeholder="Nome do participante"
+            placeholderTextColor="#FFF"
+            />
+
+          <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Clique aqui</Text>
+          </TouchableOpacity>
+        </View>
+      
     </SafeAreaView>
   );
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,17 +1,54 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'black',
-        flex: 1,
-        alignContent: "center"
-      },
-      text: {
-        color: 'white',
-        fontWeight: "bold",
-        marginTop: 48, 
-        fontSize: 24
-      }
+	container: {
+			backgroundColor: '#00623B',
+			flex: 1,
+			alignContent: "center"
+	},
+	contents:{
+		backgroundColor: '#FFF',
+		flex: 0,
+		height: 300,
+		margin: 2,
+		marginHorizontal: 9, 
+		padding: 5,
+		borderRadius: 20
+	},
+	h1: {
+		fontSize: 30,
+		fontWeight: "bold",
+		color: '#FFFF',
+		marginHorizontal: 10
+	},
+	h2: {
+		fontSize: 25, 
+		fontWeight: "bold",
+	},
+	input: {
+		backgroundColor: '#00623B',
+		borderRadius: 30, 
+		color: '#FFF',
+		height: 56,
+		padding: 16,
+		margin: 12,
+		marginHorizontal: 5,
+		fontSize: 25,
+		fontWeight: "bold",
+	},
+	button: {
+		borderRadius: 30, 
+		margin: 5,
+		backgroundColor: '#00623B',
+		height: 40,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 24,
+		fontWeight: "bold"
+	}
   });
   
   export default styles;
